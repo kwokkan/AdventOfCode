d997cfdd7d63ebb88876bebf0b8c531dcc173084
kwokkan <kwokkan@users.noreply.github.com>
2020-12-03 21:20:02 +0000

    Add puzzle 3 solution.

---
+++ Puzzles/Puzzle3.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Puzzles
+{
+    public class Puzzle3 : IPuzzle
+    {
+        private static int GetCount(int right, int down)
+        {
+            var lines = Puzzle3Input.Input;
+
+            var count = 0;
+            var currentPosition = right;
+
+            for (int i = down; i < lines.Length; i += down)
+            {
+                var line = lines[i];
+
+                if (line[currentPosition] == '#')
+                {
+                    count++;
+                }
+
+                currentPosition += right;
+
+                currentPosition %= line.Length;
+            }
+
+            return count;
+        }
+
+        private static string Solve1()
+        {
+            var count = GetCount(3, 1);
+
+            return count.ToString();
+        }
+
+        private static string Solve2()
+        {
+            var counts = new List<long>
+            {
+                GetCount(1, 1),
+                GetCount(3, 1),
+                GetCount(5, 1),
+                GetCount(7, 1),
+                GetCount(1, 2),
+            };
+
+            return counts.Aggregate((x, y) => x * y).ToString();
+        }
+
+        Solution IPuzzle.Solve()
+        {
+            return new Solution
+            (
+                Solve1(),
+                Solve2()
+            );
+        }
+    }
+}
