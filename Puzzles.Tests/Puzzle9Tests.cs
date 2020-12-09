be2dea0959052d16d86f8b6be14228227ab12057
kwokkan <kwokkan@users.noreply.github.com>
2020-12-09 17:50:24 +0000

    Add puzzle 9 solution.

---
+++ Puzzles.Tests/Puzzle9Tests.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2020.Puzzles.Tests
+{
+    public class Puzzle9Tests : TestBase<Puzzle9>
+    {
+        public Puzzle9Tests()
+            : base(127, 257342611, 35602097)
+        {
+        }
+    }
+}
---
+++ Puzzles/Puzzle9.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Puzzles
+{
+    public class Puzzle9 : IPuzzle
+    {
+        private static bool HasSum(IList<long> values, long sum)
+        {
+            for (int i = 0; i < values.Count; i++)
+            {
+                for (int j = 0; j < values.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if (values[i] + values[j] == sum)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static long GetEarliest(long[] values, int preamble)
+        {
+            var earliest = 0L;
+
+            for (int i = preamble; i < values.Length; i++)
+            {
+                var currentValue = values[i];
+
+                var prevValues = new List<long>();
+
+                for (int j = i - 1; j >= i - preamble; j--)
+                {
+                    prevValues.Add(values[j]);
+                }
+
+                if (!HasSum(prevValues, currentValue))
+                {
+                    earliest = currentValue;
+                    break;
+                }
+            }
+
+            return earliest;
+        }
+
+        private static long Sample()
+        {
+            return GetEarliest(Puzzle9Input.Sample, 5);
+        }
+
+        private static long Solve1()
+        {
+            var earliest = GetEarliest(Puzzle9Input.Input, 25);
+
+            return earliest;
+        }
+
+        private static long Solve2()
+        {
+            var earliest = GetEarliest(Puzzle9Input.Input, 25);
+
+            var values = Puzzle9Input.Input;
+
+            var counted = new List<long>(values.Length);
+
+            var sum = 0L;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                counted.Clear();
+
+                var found = false;
+
+                for (int j = i; j < values.Length; j++)
+                {
+                    counted.Add(values[j]);
+
+                    var currentSum = counted.Sum();
+
+                    if (currentSum == earliest && counted.Count > 2)
+                    {
+                        found = true;
+                        break;
+                    }
+                    if (currentSum > earliest)
+                    {
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    sum = counted.Min() + counted.Max();
+                    break;
+                }
+            }
+
+            return sum;
+        }
+
+        Solution IPuzzle.Solve()
+        {
+            return new Solution
+            (
+                Sample(),
+                Solve1(),
+                Solve2()
+            );
+        }
+    }
+}
