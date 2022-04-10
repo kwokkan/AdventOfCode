eade13e77aca74717fe02e62d9163b36cd6b434f
kwokkan <kwokkan@users.noreply.github.com>
2022-04-10 22:49:34 +0100

    Add 2015 day 1.

---
+++ src/Puzzles2015/Puzzle1.cs
@@ -0,0 +1,74 @@
+﻿using AdventOfCode.PuzzleCore;
+
+namespace AdventOfCode.Puzzles2015;
+
+public class Puzzle1 : PuzzleBase
+{
+    public Puzzle1()
+        : base(2015, 1)
+    {
+    }
+
+    private static long Calculate1(string input)
+    {
+        return input.Count(x => x == '(') - input.Count(x => x == ')');
+    }
+
+    private static long Calculate2(string input)
+    {
+        var currrent = 0;
+        var position = 0;
+
+        foreach (var dir in input)
+        {
+            position++;
+
+            switch (dir)
+            {
+                case '(':
+                    currrent++;
+                    break;
+                case ')':
+                    currrent--;
+                    break;
+                default:
+                    break;
+            }
+
+            if (currrent == -1)
+            {
+                return position;
+            }
+        }
+
+        return position;
+    }
+
+    public override long Sample1()
+    {
+        var input = GetSampleString()[0];
+
+        return Calculate1(input);
+    }
+
+    public override long Sample2()
+    {
+        var input = GetSampleString()[0];
+
+        return Calculate2(input);
+    }
+
+    public override long Solve1()
+    {
+        var input = GetInputString()[0];
+
+        return Calculate1(input);
+    }
+
+    public override long Solve2()
+    {
+        var input = GetInputString()[0];
+
+        return Calculate2(input);
+    }
+}
---
+++ tests/Puzzles2015.Tests/PuzzleTests.cs
@@ -0,0 +1,11 @@
+﻿using AdventOfCode.PuzzleTestCore;
+
+namespace AdventOfCode.Puzzles2015.Tests;
+
+public class Puzzle1Tests : TestBase<Puzzle1>
+{
+    public Puzzle1Tests()
+        : base(-3, 1, 280, 1797)
+    {
+    }
+}
